23f6e11743fef87fd78442b9202e1b8b808d63bc
William Stuckey <guyfleeman@gmail.com>
2014-06-28 00:51:14 -0400

    finished hashing and crypto system for file structure

---
+++ bin/README.txt
@@ -0,0 +1,5 @@
+This folder contains finished executables for the VaSolSim. If you are looking to download the program you are in the right spot.
+
+The .jar files require Java 7 or later to run.
+
+They follow the naming convention (major version).(minor version).(build number)-VaSolSim.jar. Generally speaking which ever (assuming there is more than one file) has the highest value for major version is the jar you want to run. As long as Java 7 is installed. Simply double click the jar file. If you have any inquiries, please email me at guyfleeman@gmail.com with the subject header "VaSOLSim help". 
--- src/com/gmail/guyfleeman/vasolsim/VaSOLSimTestRunner.java
+++ src/com/gmail/guyfleeman/vasolsim/VaSOLSimTestRunner.java
@@ -1,6 +1,9 @@
 package com.gmail.guyfleeman.vasolsim;
 
 
+import com.gmail.guyfleeman.vasolsim.common.file.VaSOLSimTestFile;
+
+import javax.crypto.Cipher;
 
 /**
  * @author guyfleeman
@@ -9,8 +12,22 @@ package com.gmail.guyfleeman.vasolsim;
  */
 public class VaSOLSimTestRunner
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
+		System.out.println(VaSOLSimTestFile.convertHashToString(VaSOLSimTestFile.generateHash("m3chdud3m@n".getBytes(), (short) 160)));
+		System.out.println(VaSOLSimTestFile.convertHashToString(VaSOLSimTestFile.generateHash("m3chdud3m@n".getBytes(), (short) 256)));
+		System.out.println(VaSOLSimTestFile.convertHashToString(VaSOLSimTestFile.generateHash("m3chdud3m@n".getBytes(), (short) 384)));
+		System.out.println(VaSOLSimTestFile.convertHashToString(VaSOLSimTestFile.generateHash("m3chdud3m@n".getBytes(), (short) 512)));
 
+		byte[] passwordHash = VaSOLSimTestFile.generateHash("m3chdud3m@n".getBytes(), (short) 256);
+		Cipher ec = VaSOLSimTestFile.getEncryptionCipher(passwordHash);
+		Cipher dc = VaSOLSimTestFile.getDecryptionCipher(passwordHash);
+
+		String message = "Hello Crypto World!";
+		System.out.println(message);
+		byte[] encMessage = ec.doFinal(message.getBytes());
+		System.out.println(new String(encMessage));
+		String decMessage = new String(dc.doFinal(encMessage));
+		System.out.println(decMessage);
 	}
 }
