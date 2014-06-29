db8ea59adb270e4c62b49764f4629357f6e16172
William Stuckey <guyfleeman@gmail.com>
2014-06-29 19:56:37 -0400

    finished vss file XML schema

--- src/com/gmail/guyfleeman/vasolsim/VaSOLSimTestRunner.java
+++ src/com/gmail/guyfleeman/vasolsim/VaSOLSimTestRunner.java
@@ -1,7 +1,7 @@
 package com.gmail.guyfleeman.vasolsim;
 
 
-import com.gmail.guyfleeman.vasolsim.common.file.VaSOLSimTestFile;
+import com.gmail.guyfleeman.vasolsim.common.file.VaSOLSimExamFile;
 
 import javax.crypto.Cipher;
 
@@ -14,14 +14,14 @@ public class VaSOLSimTestRunner
 {
 	public static void main(String[] args) throws Exception
 	{
-		System.out.println(VaSOLSimTestFile.convertHashToString(VaSOLSimTestFile.generateHash("m3chdud3m@n".getBytes(), (short) 160)));
-		System.out.println(VaSOLSimTestFile.convertHashToString(VaSOLSimTestFile.generateHash("m3chdud3m@n".getBytes(), (short) 256)));
-		System.out.println(VaSOLSimTestFile.convertHashToString(VaSOLSimTestFile.generateHash("m3chdud3m@n".getBytes(), (short) 384)));
-		System.out.println(VaSOLSimTestFile.convertHashToString(VaSOLSimTestFile.generateHash("m3chdud3m@n".getBytes(), (short) 512)));
+		System.out.println(VaSOLSimExamFile.convertHashToString(VaSOLSimExamFile.generateHash("m3chdud3m@n".getBytes(), (short) 160)));
+		System.out.println(VaSOLSimExamFile.convertHashToString(VaSOLSimExamFile.generateHash("m3chdud3m@n".getBytes(), (short) 256)));
+		System.out.println(VaSOLSimExamFile.convertHashToString(VaSOLSimExamFile.generateHash("m3chdud3m@n".getBytes(), (short) 384)));
+		System.out.println(VaSOLSimExamFile.convertHashToString(VaSOLSimExamFile.generateHash("m3chdud3m@n".getBytes(), (short) 512)));
 
-		byte[] passwordHash = VaSOLSimTestFile.generateHash("m3chdud3m@n".getBytes(), (short) 256);
-		Cipher ec = VaSOLSimTestFile.getEncryptionCipher(passwordHash);
-		Cipher dc = VaSOLSimTestFile.getDecryptionCipher(passwordHash);
+		byte[] passwordHash = VaSOLSimExamFile.generateHash("m3chdud3m@n".getBytes(), (short) 256);
+		Cipher ec = VaSOLSimExamFile.getEncryptionCipher(passwordHash);
+		Cipher dc = VaSOLSimExamFile.getDecryptionCipher(passwordHash);
 
 		String message = "Hello Crypto World!";
 		System.out.println(message);
