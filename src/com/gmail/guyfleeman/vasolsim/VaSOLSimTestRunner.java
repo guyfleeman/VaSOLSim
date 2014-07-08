d964df7095548ac6d99fdf6459e10525f31265de
William Stuckey <guyfleeman@gmail.com>
2014-07-07 21:33:49 -0400

    finished primary file writing capability

--- src/com/gmail/guyfleeman/vasolsim/VaSOLSimTestRunner.java
+++ src/com/gmail/guyfleeman/vasolsim/VaSOLSimTestRunner.java
@@ -1,9 +1,9 @@
 package com.gmail.guyfleeman.vasolsim;
 
-
-import com.gmail.guyfleeman.vasolsim.common.file.VaSOLSimExamFile;
+import com.gmail.guyfleeman.vasolsim.common.file.VaSOLSimExam;
 
 import javax.crypto.Cipher;
+import java.io.File;
 
 /**
  * @author guyfleeman
@@ -14,14 +14,18 @@ public class VaSOLSimTestRunner
 {
 	public static void main(String[] args) throws Exception
 	{
-		System.out.println(VaSOLSimExamFile.convertHashToString(VaSOLSimExamFile.generateHash("m3chdud3m@n".getBytes(), (short) 160)));
-		System.out.println(VaSOLSimExamFile.convertHashToString(VaSOLSimExamFile.generateHash("m3chdud3m@n".getBytes(), (short) 256)));
-		System.out.println(VaSOLSimExamFile.convertHashToString(VaSOLSimExamFile.generateHash("m3chdud3m@n".getBytes(), (short) 384)));
-		System.out.println(VaSOLSimExamFile.convertHashToString(VaSOLSimExamFile.generateHash("m3chdud3m@n".getBytes(), (short) 512)));
+		docGen();
 
-		byte[] passwordHash = VaSOLSimExamFile.generateHash("m3chdud3m@n".getBytes(), (short) 256);
-		Cipher ec = VaSOLSimExamFile.getEncryptionCipher(passwordHash);
-		Cipher dc = VaSOLSimExamFile.getDecryptionCipher(passwordHash);
+		//System.out.println(VaSOLSimExam.convertHashToString(VaSOLSimExam.generateHash("m3chdud3m@n".getBytes(), (short) 512)));
+		//System.out.println(VaSOLSimExam.convertHashToString(VaSOLSimExam.generateHash("m3chdud3m@n".getBytes(), (short) 512)));
+
+		/*
+		System.out.println(VaSOLSimExam.convertHashToString(VaSOLSimExam.generateHash("m3chdud3m@n".getBytes(), VaSOLSimExam.HashType.SHA256)));
+		System.out.println(VaSOLSimExam.convertHashToString(VaSOLSimExam.generateHash("m3chdud3m@n".getBytes(), VaSOLSimExam.HashType.SHA512)));
+
+		byte[] passwordHash = VaSOLSimExam.generateHash("m3chdud3m@n".getBytes(), VaSOLSimExam.HashType.SHA256);
+		Cipher ec = VaSOLSimExam.getEncryptionCipher(passwordHash);
+		Cipher dc = VaSOLSimExam.getDecryptionCipher(passwordHash);
 
 		String message = "Hello Crypto World!";
 		System.out.println(message);
@@ -29,5 +33,26 @@ public class VaSOLSimTestRunner
 		System.out.println(new String(encMessage));
 		String decMessage = new String(dc.doFinal(encMessage));
 		System.out.println(decMessage);
+		*/
+	}
+
+	public static void docGen() throws Exception
+	{
+		VaSOLSimExam exam = new VaSOLSimExam();
+		exam.setReportingStatistics(true);
+		exam.setReportingStatisticsEncrypted(true);
+		exam.setReportingStatisticsUsingStandaloneEmailParadigm(true);
+		exam.setDecryptedStatisticsEmail("guyfleeman@gmail.com");
+		exam.setDecryptedStatisticsEmailPassword("guyfleeman".getBytes());
+
+		exam.setNotifyingCompletion(true);
+		exam.setDecryptedNotificationEmail("guyfleeman@gmail.com");
+		exam.setDecryptedNotificationEmailPassword("guyfleeman".getBytes());
+
+		exam.setValidationKey("m3chdud3m@n");
+
+		exam.setTestPublisher("Will S.");
+
+		exam.write(new File("/home/williamstuckey/Desktop/exam.xml"), true);
 	}
 }
